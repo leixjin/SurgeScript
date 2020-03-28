d054f53a0a6b939ff2357bcf95c407024f8d70b7
金小白 <leixjin@gmail.com>
2020-03-29 00:30:15 +0800

    Create ok_full.js

---
+++ ok_full.js
@@ -0,0 +1,12 @@
+// 获取返回内容
+let body = $response.body;
+// To JSON
+let res = JSON.parse(body);
+// 修改返回值
+res.data.international = 0;
+res.data.inReview = 0;
+// To String
+body = JSON.stringify(res);
+console.log("---------------------------");
+// Callback
+$done({ body });
